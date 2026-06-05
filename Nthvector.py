0e021fae33a8341de2e11bf09eb83061a9fa58f2
atticusmgraham-create <atticusmgraham@gmail.com>
2026-06-05 14:03:20 -0600

    Implement QR decomposition and rank calculation
    Added matrix_rank import and implemented QR decomposition for basis matrices.

--- Nthvector.py
+++ Nthvector.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+from numpy.linalg import matrix_rank
 def vect(o,p,i,a,x):
  import math
  angle=[p,i,x,a]
@@ -27,3 +28,35 @@ psia = psia / np.linalg.norm(psia)
 psid =psid/np.linalg.norm(psid)
 print("state vector A: ",psia)
 print("state vector B: ",psid)
+def projection(psi, i):
+    return psi[i]
+basisa=np.eye(len(psia))
+basisb=np.eye(len(psid))
+#print(basisb)
+#print(basisa)
+
+
+rank = matrix_rank(basisa)
+
+#print("Dimension of basis:", rank)
+Q, R = np.linalg.qr(basisa.T)  # QR decomposition
+
+basis = Q[:, :np.linalg.matrix_rank(basisa)]
+print("m1:")
+print(basis)
+rankd = matrix_rank(basisb)
+
+#print("Dimension of basis:", rank)
+Q, R = np.linalg.qr(basisb.T)  # QR decomposition
+
+basisu = Q[:, :np.linalg.matrix_rank(basisb)]
+print("m2:")
+print(basisu)
+
+#gs=projection(psia, 2) # amplitude of |2>
+#tb=(projection(psid, 2))  
+#a = np.array([1, 2, 3])   # system A (n=3)
+#b = np.array([4, 5, 6])   # system B (n=3)
+#psi = np.kron(a, b)  # n^2-dimensional state
+#n = 3
+#psi_matrix = psi.reshape
